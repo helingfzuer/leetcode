2f36f621070fe7dae383388e5d22e96a6c7e94ed
helingfzuer <76670749+helingfzuer@users.noreply.github.com>
2021-11-26 23:41:14 +0800

    Delete huaWeiExam directory

--- huaWeiExam/HJ2.py
+++
@@ -1,4 +0,0 @@
-if __name__ == '__main__':
-	s = input().lower()
-	char = input().lower()
-	print(s.count(char))
--- huaWeiExam/HJ9.py
+++
@@ -1,7 +0,0 @@
-if __name__ == '__main__':
-	n = input()
-	res = ''
-	for num in n[: : -1]:
-		if num not in res:
-			res += num
-	print(res)
